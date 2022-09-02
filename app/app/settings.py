88191353e92ce05aad8abd40a709fbb084d5383e
ks982579 <kevinsullivan@ksulldev.space>
2022-09-02 08:29:16 +0100

    setting up PostgreSQL database in settings.py file

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': environ.get('DB_HOST'),
+        'NAME': environ.get('DB_NAME'),
+        'USER': environ.get('DB_USER'),
+        'PASSWORD': environ.get('DB_PASS'),
     }
 }
 
