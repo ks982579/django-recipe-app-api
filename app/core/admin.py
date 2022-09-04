8cab3c83ce92dcb27fafee6ac74d8b01b14c0164
ks982579 <kevinsullivan@ksulldev.space>
2022-09-04 13:07:06 +0100

    Fixed up the Django Admin panel to properly connect with our custom User Model.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,50 @@
-from django.contrib import admin
+"""
+Django admin customization
+"""
 
-# Register your models here.
+from array import array
+from typing import List
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            gettext_lazy('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (
+            gettext_lazy('Important dates'),
+            {'fields':('last_login',)},
+        )
+    )
+    readonly_fields: list = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
