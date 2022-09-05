bc0fe29df36b03d27a3ffbdff572c7fbec84417f
ks982579 <kevinsullivan@ksulldev.space>
2022-09-05 08:28:30 +0100

    adding tests for creating new user

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,37 @@
+"""
+Tests for the user API.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+# returns full URL path
+CREATE_USER_URL = reverse("user:create")
+
+def create_user(**params):
+    """Create and return a new User."""
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserApiTests(TestCase):
+    """Test the public features of the user API."""
+    def setUp(self):
+        self.client = APIClient()
+    
+    def test_create_user_success(self):
+        """Test creating a user is successful."""
+        # Payload to pass in
+        payload = {
+            'email': 'test@example.com',
+            'password': 'password+123',
+            'name': 'John Doe',
+        }
+        # Post Data to endpoint
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
