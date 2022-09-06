4e0cc442dcd725b27da6f7b280245ad7ce663ccc
ks982579 <kevinsullivan@ksulldev.space>
2022-09-06 08:21:04 +0100

    Tests created for User API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -35,3 +35,32 @@ class PublicUserApiTests(TestCase):
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
+    
+    def test_user_with_email_exists_error(self):
+        """Test error returned if user with email exists"""
+        # same as previous payload / should we make it class variable?
+        payload = {
+            'email': 'test@example.com',
+            'password': 'password+123',
+            'name': 'John Doe',
+        }
+        # pass dictionary in as kwargs
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_password_too_short_error(self):
+        """Test an error is returned if password is less than 5 characters."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'abc',
+            'name': 'John Doe',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
