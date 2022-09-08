3b2955addbcb14d1d5f0aab8a9df6f3ffebfbe1b
ks982579 <kevinsullivan@ksulldev.space>
2022-09-08 08:43:22 +0100

    cleaning up syntax

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,17 +1,4 @@
 """
-Sample Tests
+Tests for main app
 """
 from django.test import SimpleTestCase
-from app import calc
-
-class CalcTests(SimpleTestCase):
-    """Test the calc Module."""
-    def test_add_numbers(self):
-        """Test Adding numbers together."""
-        res = calc.add(5, 6)
-        self.assertEqual(res, 11)
-    
-    def test_subtract_numbers(self):
-        """Test subtracting numbers."""
-        res = calc.subtract(10, 15)
-        self.assertEqual(res, 5)
