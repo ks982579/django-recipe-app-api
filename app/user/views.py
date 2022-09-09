4f89efec8bebe24ae7c4830e3f03860019bcf477
ks982579 <kevinsullivan@ksulldev.space>
2022-09-09 09:35:10 +0100

    Added view to implement Token API - All tests passing

--- app/user/views.py
+++ app/user/views.py
@@ -4,10 +4,17 @@ Views for the user API.
 
 from django.shortcuts import render
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 # Create your views here.
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+class CreateTokeView(ObtainAuthToken):
+    """Create a new auth token for the user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
