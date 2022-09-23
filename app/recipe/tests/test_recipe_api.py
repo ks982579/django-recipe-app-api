5de0ba8b0dbfa5f7eb133e1ea7a7a68720f76ea7
ks982579 <kevinsullivan@ksulldev.space>
2022-09-23 09:31:20 +0100

    creating tests for our recipe endpoint

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,45 @@
+"""
+Test for recipe APIs.
+"""
+
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe
+from recipe.serializers import RecipeSerializer
+
+RECIPES_URL = reverse('recipe:recipe-list')
+
+def create_recipe(user, **params):
+    """Create and return a sample recipe."""
+    defaults = {
+        'title': 'Sample recipe title',
+        'time_minutes': 22,
+        'price': Decimal(10.69),
+        'description': 'Sample Description, super yummy.',
+        'link': 'http://example.com/recipe.pdf',
+    }
+    defaults.update(params)
+
+    recipe = Recipe.objects.create(user=user, **defaults)
+    return recipe
+
+class PublicRecipeAPITests(TestCase):
+    """Test unauthenticated API requests."""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required to call API."""
+        res = self.client.get(RECIPES_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateRecipeAPITests(TestCase):
+    """Test authenticated API requests."""
