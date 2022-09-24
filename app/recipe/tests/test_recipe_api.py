6714240f25faa617c38b6339a1d9a78e08f48cf3
ks982579 <kevinsullivan@ksulldev.space>
2022-09-24 15:00:05 +0100

    complete GET recipe testing

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -43,3 +43,44 @@ class PublicRecipeAPITests(TestCase):
 
 class PrivateRecipeAPITests(TestCase):
     """Test authenticated API requests."""
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'user@example.com',
+            'testpass123',
+        )
+        self.client.force_authenticate(self.user)
+    
+    def test_retrieve_recipes(self):
+        """Test retrieving a list of recipes."""
+        create_recipe(user=self.user)
+        create_recipe(user=self.user, title='second recipe')
+        create_recipe(user=self.user, title='third recipe', time_minutes=20)
+        
+        res = self.client.get(RECIPES_URL)
+
+        recipes = Recipe.objects.all().order_by('-id')
+        serializer = RecipeSerializer(recipes, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+    
+    def test_recipe_list_limited_to_user(self):
+        """Test list of recipes is limited to authenticated user."""
+        other_user = get_user_model().objects.create_user(
+            'other_guy@example.com',
+            'testpass1234',
+        )
+        create_recipe(user=other_user)
+        create_recipe(user=self.user, title='second recipe')
+        create_recipe(user=self.user, title='third recipe', time_minutes=20)
+
+        # Make GET request to RECIPES_URL
+        res = self.client.get(RECIPES_URL)
+
+        recipes = Recipe.objects.filter(user=self.user).order_by('-id')
+        serialized_recipes = RecipeSerializer(recipes, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serialized_recipes.data)
+        
