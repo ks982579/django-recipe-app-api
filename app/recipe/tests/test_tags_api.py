9e810fe7da0f233d766d8ba5d0b461a1581c12b4
ks982579 <kevinsullivan@ksulldev.space>
2022-09-27 21:03:14 +0100

    Update urls for tags and fixing errors

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -17,7 +17,7 @@ TAGS_URL = reverse('recipe:tag-list')
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a user."""
-    return get_user_model().objects().create_user(email, password)
+    return get_user_model().objects.create_user(email, password)
 
 class PublicTagsApiTests(TestCase):
     """Test unauthenticated API requests."""
