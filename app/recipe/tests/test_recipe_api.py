92efbe9b33604e186cb487e2f59403c777992353
ks982579 <kevinsullivan@ksulldev.space>
2022-09-29 08:26:56 +0100

    Added testing for creating recipe with new tags

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -3,6 +3,7 @@ Test for recipe APIs.
 """
 
 from decimal import Decimal
+from typing import Reversible
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -11,7 +12,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import (Recipe, Tag)
 from recipe.serializers import (
     RecipeSerializer,
     RecipeDetailSerializer,
@@ -215,3 +216,27 @@ class PrivateRecipeAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
         self.assertTrue(Recipe.objects.filter(pk=recipe.id).exists())
+    
+    def create_recipe_with_new_tags(self) -> None:
+        """Test creating a recipe with new tags."""
+        def payload_maker(**kwargs) -> dict:
+            return kwargs
+        payload = payload_maker(
+            title='Thai Prawn Curry',
+            time_minutes=30,
+            price=Decimal('12.50'),
+            tags=[{'name': 'Thai'}, {'name': 'Dinner'}]
+        )
+        res = self.client.post(RECIPES_URL, payload, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipes = Recipe.objects.filter(user=self.user)
+        self.assertEqual(recipes.count(), 1)
+        recipe = recipes[0]
+        self.assertEqual(recipe.tags.count(), 2)
+        for tag in payload['tags']:
+            exists = recipe.tags.filter(
+                name=tag['name'],
+                user=self.user,
+            ).exists()
+            self.assertTrue(exists)
