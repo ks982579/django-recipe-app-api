1b2200b656362f6d2804106ca4d5599ff197ed99
ks982579 <kevinsullivan@ksulldev.space>
2022-09-30 09:32:39 +0100

    Added feature for recipes to create tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,22 +2,9 @@
 Serializers for recipe APIs.
 """
 
+from attr import validate
 from rest_framework import serializers
 from core.models import (Recipe, Tag)
-
-class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for recipes."""
-    class Meta:
-        model = Recipe
-        #fields = []
-        exclude = ['user', 'description']
-        read_only_fields = ['id']
-
-class RecipeDetailSerializer(RecipeSerializer):
-    """Serializer for recipe detail view."""
-    class Meta(RecipeSerializer.Meta):
-        # fields = RecipeSerializer.Meta.fields + ['description']
-        exclude = ['user']
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tags."""
@@ -25,3 +12,33 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'name']
         read_only_fields = ['id']
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        #fields = []
+        exclude = ['user', 'description']
+        read_only_fields = ['id']
+
+    def create(self, validated_data) -> Recipe:
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+        
+        return recipe
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+    class Meta(RecipeSerializer.Meta):
+        # fields = RecipeSerializer.Meta.fields + ['description']
+        exclude = ['user']
