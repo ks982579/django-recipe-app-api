bdcf733f2de60463abfa67959424b0ef4c9cf75f
ks982579 <kevinsullivan@ksulldev.space>
2022-10-02 17:54:59 +0100

    Added testing for Ingredients API, public and private endpoints

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -27,3 +27,41 @@ class PublicIngredientsApiTests(TestCase):
         """Test auth is required for retrieving ingredients list."""
         res = self.client.get(INGREDIENTS_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateIngredientsApiTests(TestCase):
+    """Test authenticated API requests."""
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user()
+        self.client.force_authenticate(self.user)
+    
+    def test_retrieve_ingredients(self):
+        """Test retrieving a list of ingredients."""
+        Ingredient.objects.create(user=self.user, name="Garlic")
+        Ingredient.objects.create(user=self.user, name="Tomato")
+        Ingredient.objects.create(user=self.user, name="Kale")
+
+        res = self.cleint.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serialized_ingredients = IngredientSerializer(ingredients, many=True)
+
+        self.assertEqual(res.data, serialized_ingredients.data)
+
+    def test_ingredients_limited_to_user(self):
+        """Test list of ingredients is limited to Authenticated user."""
+        user2 = create_user(email="user2@example.com", password="Abc123")
+
+        ingredient1 = Ingredient.objects.create(user=self.user, name="salt")
+        ingredient2 = Ingredient.objects.create(user=user2, name="pepper")
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+            # check that there is only one ingredient returned, not two
+        self.assertEqual(res.data[0]['name'], ingredient1.name)
+        self.assertEqual(res.data[0]['id'], ingredient1.id)
+            # Check data of ingredient returned. 
