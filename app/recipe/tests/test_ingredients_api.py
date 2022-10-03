dda5764303b2af7262d00e34967a48c661c070be
ks982579 <kevinsullivan@ksulldev.space>
2022-10-03 09:06:55 +0100

    added testing for updating ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -2,6 +2,7 @@
 Tests for the ingredients API
 """
 
+from sys import stdout
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
@@ -12,7 +13,16 @@ from rest_framework.test import APIClient
 from core.models import Ingredient, User
 from recipe.serializers import IngredientSerializer
 
-INGREDIENTS_URL = reverse('recipe:ingredients-list')
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+def create_payload(**kwargs):
+    """Creates and returns a dictionary."""
+    return kwargs
+
+def detail_url(ingredient_id):
+    """Create and return an ingredient detail URL."""
+    return reverse('recipe:ingredient-detail', args=[ingredient_id])
+
 
 def create_user(email="email@example.com", password="TestPass+123") -> User:
     """Create and return user."""
@@ -41,7 +51,7 @@ class PrivateIngredientsApiTests(TestCase):
         Ingredient.objects.create(user=self.user, name="Tomato")
         Ingredient.objects.create(user=self.user, name="Kale")
 
-        res = self.cleint.get(INGREDIENTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
@@ -65,3 +75,24 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.data[0]['name'], ingredient1.name)
         self.assertEqual(res.data[0]['id'], ingredient1.id)
             # Check data of ingredient returned. 
+    
+    def test_update_ingredient(self):
+        """Test updating an ingredient."""
+        ingredient = Ingredient.objects.create(user=self.user, name='Garlic')
+            # Create init ingredient
+        payload = create_payload(name='Ginger')
+            # Create payload to send
+        update_url = detail_url(ingredient.pk)
+            # Create PATH with PK value
+        res = self.client.patch(update_url, payload, format='json')
+            # PATCH update to endpoint
+        # stdout.write(f'{dir(res.data)}\n')
+        # stdout.write(f'{res.data.items()}\n')
+        
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+            # Check the status returned is 200
+        ingredient.refresh_from_db()
+            # update value from DB
+        self.assertEqual(payload['name'], ingredient.name)
+        self.assertEqual(res.data.get('id'), ingredient.pk)
+        
