41e98c27b76abdf3b1a7427437f535cb69c8c2a1
ks982579 <kevinsullivan@ksulldev.space>
2022-10-08 16:31:14 +0100

    updated RecipeSerializer for clearing ingredients feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -74,6 +74,11 @@ class RecipeSerializer(serializers.ModelSerializer):
                 # remove old tags
             self._get_or_create_tags(tags, instance)
                 # create or assign tags as needed
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
+
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         
