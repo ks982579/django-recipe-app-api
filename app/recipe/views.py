741c4e6d7f087ed2a18048b5554931090fe99dc6
ks982579 <kevinsullivan@ksulldev.space>
2022-10-14 21:34:19 +0100

    Added documentation decorator.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -16,6 +16,22 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (Recipe, Tag, Ingredient)
 from recipe import serializers
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags',
+                OpenApiTypes.STR,
+                description='Comma separated list of tag IDs to filter',
+            ),
+            OpenApiParameter(
+                'ingredients',
+                OpenApiTypes.STR,
+                description='Comma separated list of ingredient IDs to filter',
+            )
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
     serializer_class = serializers.RecipeDetailSerializer
